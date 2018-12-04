73ef8acdfea2d97f3497c1c2cf2ed2b5af068b6a
Tommy Haugaard <tha@easv.dk>
2018-12-05 00:16:57 +0100

    Third

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,37 @@
 package sample;
 
-public class Controller {
-    //nmsbcbsbmbcas
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
-    public void do(){
+public class Controller {
+
+    @FXML
+    private Canvas backg, foreg;
+
+
+    @FXML
+    private void init(){
+        Square s = new Square(50,Color.BLACK,100,100);
+        drawSquare(s);
 
     }
+    /**
+     * Draws a single square on the background
+     * @param s the square to be drawn
+     */
+    private void drawSquare(Square s)
+    {
+        GraphicsContext gc = backg.getGraphicsContext2D();
+        gc.setFill(s.getColor());
+        gc.fillRect(s.getPosX(), s.getPosY(), s.getSize(), s.getSize());
+    }
+
+
 }
---
+++ src/sample/Square.java
@@ -0,0 +1,76 @@
+package sample;
+
+
+/**
+ * Created by tha on 04-12-2018.
+ * @author tha
+ */
+
+import javafx.scene.paint.Color;
+
+
+public class Square {
+    /**
+     * The size of the square
+     * In pixels
+     */
+    private int size;
+
+    private Color color;
+
+    private int posX;
+
+    private int posY;
+
+    // private Piece piece;
+
+    public Square(int size, Color color, int posX, int posY) {
+        this.size = size;
+        this.color = color;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    /*
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+    */
+}
+
